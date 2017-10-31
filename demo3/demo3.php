d8f99cf9f32a311e7bd95e5c4d4ef6195730f43e
LinZhengyu <lzy233@126.com>
2017-10-31 14:11:05 +0800

    增加了 demo2 demo3

---
+++ demo3/demo3.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <!-- 在HTML标记对中嵌入PHP脚本，使用echo()输出标题 -->
+        <title> <?php echo "PHP语言标记的使用" ?> </title>
+    </head>
+
+        <!-- 可以在HTML属性位置出嵌入PHP脚本，使用echo()输出网页背景颜色 -->
+    <body <?php echo 'background-color=#cccccc"' ?> >
+
+    <!-- 以下是在HTML中更高级的分离术 -->
+    <?php
+        if ($expression) {
+    ?>
+        <!-- 也可以在HTML标记属性的双引号中嵌入PHP标记 -->
+        <p align = "<?php echo 'center' ?>" >This is true.</p>
+    <?php
+        } else {
+    ?>
+            <p>This is false.</p>
+    <?php
+        }
+    ?>
+    
+    </body>
+
+</html>
