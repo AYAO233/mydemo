256d8ae4b05bc826d0387c5715bdbace41e4fce6
ayao233 <lzy233@126.com>
2018-01-16 18:36:27 +0800

    11

---
+++ demo5/demo10.php
@@ -0,0 +1,11 @@
+<?php
+    function myfun1($v) {
+        if ($v == "MySQL") {
+            return "Oracle";
+        }
+        return $v;
+    }
+
+    $lamp = array("Linux", "Apache", "MySQL", "PHP");
+
+    print_r(array_map(""));
